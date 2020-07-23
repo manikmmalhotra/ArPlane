011877cdaa13d35589d694a12922b11dd9340384
manik malhotra <manikmalhotra2113@gmail.com>
2020-07-23 17:33:28 +0530

    assests
    includes assets and apk file

---
+++ Assets/script/ARTapToPlace.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class ARTapToPlace : MonoBehaviour
+{
+
+    public GameObject Gameobj;
+    private GameObject spawnedObj;
+    private ARRaycastManager arraymanger;
+    private Vector2 touchPose;
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+        arraymanger = GetComponent<ARRaycastManager>();
+    }
+
+   bool TryTouchPosition(out Vector2 touchon)
+    {
+        if(Input.touchCount > 0)
+        {
+            touchPose = Input.GetTouch(0).position;
+            touchon = default;
+            return true;
+        }
+
+        touchPose = default;
+        touchon = default;
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TryTouchPosition(out touchPose))
+            return;
+
+        if (arraymanger.Raycast(touchPose, hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitpose = hits[0].pose;
+
+            if (spawnedObj == null)
+            {
+                spawnedObj = Instantiate(Gameobj, hitpose.position, hitpose.rotation);
+            }
+        }
+    }
+
+}
